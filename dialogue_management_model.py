dc66d37275da26e5781fdfc97f5fd46eb3f90ffd
David Yang <David.Yang@activenetwork.com>
2018-12-20 14:12:17 +0800

    dialogue management model

--- dialogue_management_model.py
+++ dialogue_management_model.py
@@ -11,6 +11,29 @@ from rasa_core.policies.memoization import MemoizationPolicy
 from rasa_core.interpreter import RasaNLUInterpreter
 from rasa_core.utils import EndpointConfig
 from rasa_core.run import serve_application
-from rasa_core import config
 
 logger = logging.getLogger(__name__)
+
+
+def train_dialogue(domain_file='weather_domain.yml',
+                   model_path='models/dialogue',
+                   train_data_file='data/stories.md'):
+    agent = Agent(domain_file, policies=[MemoizationPolicy(),
+                                         KerasPolicy(max_history=3, epochs=200, batch_size=50)])
+    data = agent.load_data(train_data_file)
+
+    agent.train(data)
+    agent.persist(model_path)
+    return agent
+
+
+def run_weather_bot():
+    interpreter = RasaNLUInterpreter('./models/nlu/default/weathernlu')
+    action_endpoint = EndpointConfig(url="http://localhost:5055/webhook")
+    agent = Agent.load('./models/dialogue', interpreter=interpreter, action_endpoint=action_endpoint)
+    rasa_core.run.serve_application(agent, channel='cmdline')
+
+
+if __name__ == '__main__':
+    train_dialogue()
+    run_weather_bot()
